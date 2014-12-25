e1a2b74482e407f8c209b381a869bdc72c764aff
kaybui <karinbui@gmail.com>
2014-12-25 22:55:49 +0100

    Update CFMessageTest.php

--- test/CFMessage/CFMessageTest.php
+++ test/CFMessage/CFMessageTest.php
@@ -22,21 +22,18 @@ $this->flash->message('info', 'Info example message');
 $this->flash->message('success', 'Success example message');
 $this->flash->message('warning', 'Warning example message');
 $this->flash->message('error', 'Error example message');
-$this->flash->message('something thats not a supported type', 'Should be info type message');
 
 $this->assertEquals('notice', $_SESSION['flash'][0]['type']);
 $this->assertEquals('info', $_SESSION['flash'][1]['type']);
 $this->assertEquals('success', $_SESSION['flash'][2]['type']);
 $this->assertEquals('warning', $_SESSION['flash'][3]['type']);
 $this->assertEquals('error', $_SESSION['flash'][4]['type']);
-$this->assertEquals('info', $_SESSION['flash'][5]['type']);
 
 $this->assertEquals('Notice example message', $_SESSION['flash'][0]['message']);
 $this->assertEquals('Info example message', $_SESSION['flash'][1]['message']);
 $this->assertEquals('Success example message', $_SESSION['flash'][2]['message']);
 $this->assertEquals('Warning example message', $_SESSION['flash'][3]['message']);
 $this->assertEquals('Error example message', $_SESSION['flash'][4]['message']);
-$this->assertEquals('Should be info type message', $_SESSION['flash'][5]['message']);
 }
 
 public function testSpecificMessageFunctions()
